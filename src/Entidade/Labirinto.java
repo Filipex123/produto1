cb832bfd771203407daf8448415bdd53e02af933
Leonardo Barros <leoguedesbarros@gmail.com>
2021-03-17 23:12:06 -0300

    Classe Labirinto Criada

---
+++ src/Entidade/Labirinto.java
@@ -0,0 +1,11 @@
+package Entidade;
+
+public class Labirinto {
+
+    private Coordenada entrada;
+    private Coordenada saida;
+    private int altura;
+    private int largura;
+    private String[][] mapa;
+
+}
