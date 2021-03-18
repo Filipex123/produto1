c9e74010f71772797cb424e67679357061dc0ea7
Leonardo Barros <leoguedesbarros@gmail.com>
2021-03-17 23:58:54 -0300

    Construtor Labirinto

--- src/Entidade/Labirinto.java
+++ src/Entidade/Labirinto.java
@@ -8,4 +8,13 @@ public class Labirinto {
     private int largura;
     private String[][] mapa;
 
+    public Labirinto(int altura, int largura, Coordenada entrada, Coordenada saida) {
+
+        this.altura = altura;
+        this.largura = largura;
+        this.entrada = entrada;
+        this.saida = saida;
+        this.mapa = new String[altura][largura];
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Entidade.Coordenada;
+import Entidade.Labirinto;
 import Entidade.Pilha;
 import Ferramenta.ArquivoUtils;
 
@@ -8,6 +9,11 @@ public class Main {
         Pilha<Coordenada> caminhos = new Pilha<>(40);
         Pilha<Coordenada> adjacentes = new Pilha<>(3);
         Pilha<Pilha<Coordenada>> possibilidades = new Pilha(40);
+
+        Coordenada coordenadaEntrada = new Coordenada(5,5);
+        Coordenada coordenadaSaida = new Coordenada(2,2);
+
+        Labirinto labirinto = new Labirinto(5,5,new Coordenada(5,5),coordenadaSaida);
 
         ArquivoUtils arq = new ArquivoUtils();
 
