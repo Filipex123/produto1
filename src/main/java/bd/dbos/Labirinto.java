942732f254f58a33cea7b0b27a76becae98ef63d
Filipe Zanelato <filipezanelato23@gmail.com>
2021-05-07 22:14:17 -0300

    neves fez o tostring

--- src/main/java/bd/dbos/Labirinto.java
+++ src/main/java/bd/dbos/Labirinto.java
@@ -9,4 +9,15 @@ public class Labirinto implements Cloneable {
     private LocalDateTime dataCriacao;
     private LocalDateTime dataEdicao;
     private String conteudo;
+
+    @Override
+    public String toString(){
+        String ret = "{\n   Nome do Labirinto: " + this.nome
+                + "\n   IP do cliente: " + this.identificador
+                + "\n   Data de Criação: " + this.dataCriacao
+                + "\n   Data de Edicao: " + this.dataEdicao
+                + "\n   Conteúdo: " + this.conteudo
+                + "\n}";
+        return ret;
+    }
 }
