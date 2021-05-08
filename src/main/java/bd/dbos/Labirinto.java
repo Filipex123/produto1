a85007a22adf7a87e44706fe2f6e8cc1f3050788
Leonardo Barros <leoguedesbarros@gmail.com>
2021-05-07 22:36:28 -0300

    Get / Set

--- src/main/java/bd/dbos/Labirinto.java
+++ src/main/java/bd/dbos/Labirinto.java
@@ -10,6 +10,62 @@ public class Labirinto implements Cloneable {
     private LocalDateTime dataEdicao;
     private String conteudo;
 
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdentificador() {
+        return this.identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return this.dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataEdicao() {
+        return this.dataEdicao;
+    }
+
+    public void setDataEdicao(LocalDateTime dataEdicao) {
+        this.dataEdicao = dataEdicao;
+    }
+
+    public String getConteudo() {
+        return this.conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+
+    @Override
+    public Object clone ()
+    {
+        Labirinto ret = null;
+
+        try
+        {
+          ret = new Labirinto(this);
+        }
+        catch (Exception erro)
+        {}
+
+        return ret;
+    }
+
     @Override
     public String toString(){
         return "{\n   Nome do Labirinto: " + this.nome
