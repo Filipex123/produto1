75e46c7369a1b0476ba60003c473b2e458851a4a
Leonardo Barros <leoguedesbarros@gmail.com>
2021-05-27 16:05:30 -0300

    Metodos Obrigatorios Pedido Salvamento e Resp Labirintos

--- src/main/java/network/entidade/PedidoSalvamento.java
+++ src/main/java/network/entidade/PedidoSalvamento.java
@@ -25,4 +25,57 @@ public class PedidoSalvamento implements Comunicado {
     public LabirintoNetworkEntity getLabirinto() {
         return labirinto;
     }
+
+
+    /**
+     * Método para impressão customizada dPedido Salvemento
+     * @return String impressão de Pedido Salvemento
+     */
+    @Override
+    public String toString(){
+        return "{\n    Labirinto: " + this.labirinto
+                + "\n}";
+    }
+
+    /**
+     * Método hashCoode de Pedido Salvemento
+     * @return int hash code
+     */
+    @Override
+    public int hashCode() {
+
+        int ret = 666;
+
+        ret = 13*ret + this.labirinto.hashCode();
+
+        return (ret<0)? -ret : ret;
+    }
+
+
+    public PedidoSalvamento(PedidoSalvamento modelo){
+        this.labirinto = modelo.labirinto;
+    }
+
+    /**
+     * Método para clone de Pedido Salvemento
+     *
+     * @return Object de Pedido Salvemento
+     */
+    @Override
+    public Object clone ()
+    {
+        PedidoSalvamento pedSalvamento = null;
+
+        try
+        {
+            pedSalvamento = new PedidoSalvamento(this);
+        }
+        catch (Exception erro)
+        {
+            System.err.println("Erro ao clonar [" + erro + "]");
+        }
+
+        return pedSalvamento;
+    }
+}
 }
