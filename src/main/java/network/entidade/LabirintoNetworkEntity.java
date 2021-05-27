bfa58e4b9e7d16693cc11441cf8b73263cdbfbbb
Leonardo Barros <leoguedesbarros@gmail.com>
2021-05-27 15:36:40 -0300

    Metodos Obrigatorios LabirintoNetwork

--- src/main/java/network/entidade/LabirintoNetworkEntity.java
+++ src/main/java/network/entidade/LabirintoNetworkEntity.java
@@ -1,5 +1,6 @@
 package network.entidade;
 
+import bd.dbos.LabirintoDBO;
 import network.entidade.base.Comunicado;
 
 import java.time.LocalDateTime;
@@ -41,5 +42,99 @@ public class LabirintoNetworkEntity implements Comunicado {
 
     public LocalDateTime getDataEdicao() {
         return dataEdicao;
+    }
+
+    public LabirintoNetworkEntity(LabirintoNetworkEntity modelo){
+        this.nome = modelo.nome;
+        this.identificador = modelo.identificador;
+        this.dataCriacao = modelo.dataCriacao;
+        this.dataEdicao = modelo.dataEdicao;
+        this.conteudo = modelo.conteudo;
+    }
+
+    /**
+     * Método para clone de Labirinto
+     *
+     * @return Object do labirinto clonado
+     */
+    @Override
+    public Object clone ()
+    {
+        LabirintoNetworkEntity labNet = null;
+
+        try
+        {
+            labNet = new LabirintoNetworkEntity(this);
+        }
+        catch (Exception erro)
+        {
+            System.err.println("Erro ao clonar [" + erro + "]");
+        }
+
+        return labNet;
+    }
+
+    /**
+     * Método para impressão customizada do Labirinto
+     * @return String impressão do objeto Labirinto
+     */
+    @Override
+    public String toString(){
+        return "{\n   Nome do Labirinto: " + this.nome
+                + "\n   IP do cliente: " + this.identificador
+                + "\n   Data de Criação: " + this.dataCriacao
+                + "\n   Data de Edicao: " + this.dataEdicao
+                + "\n   Conteúdo: " + this.conteudo
+                + "\n}";
+    }
+
+    /**
+     * Método hashCoode de Labirinto
+     * @return int hash code
+     */
+    @Override
+    public int hashCode() {
+
+        int ret = 666;
+
+        ret = 13*ret + this.nome.hashCode();
+        ret = 13*ret + this.identificador.hashCode();
+        ret = 13*ret + this.dataCriacao.hashCode();
+        ret = 13*ret + this.dataEdicao.hashCode();
+        ret = 13*ret + this.conteudo.hashCode();
+
+        return (ret<0)? -ret : ret;
+    }
+
+    /**
+     * Método equals para o Labirinto
+     * @param obj outro objeto a ser comparado
+     * @return boolean do resultado
+     */
+    @Override
+    public boolean equals(Object obj) {
+
+        if(this == obj) {
+            return true;
+        }
+
+        if(obj == null) {
+            return false;
+        }
+
+        if(!(obj instanceof LabirintoNetworkEntity)) {
+            return false;
+        }
+
+        LabirintoNetworkEntity labNet = (LabirintoNetworkEntity)obj;
+
+        if(!this.nome.equals(labNet.nome)
+                && !this.identificador.equals(labNet.identificador)
+                && !this.dataCriacao.equals(((LabirintoNetworkEntity) obj).dataCriacao)
+                && !this.dataEdicao.equals(((LabirintoNetworkEntity) obj).dataEdicao)
+                && !this.conteudo.equals(labNet.conteudo)){
+            return false;
+        }
+        return true;
     }
 }
